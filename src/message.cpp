fce910b0a11d574e243e4048fe4e23c6363185ed
yaogod <823908345@qq.com>
2024-06-27 17:52:56 +0800

    v1

--- src/message.cpp
+++ src/message.cpp
@@ -1,1 +1,49 @@
-#include "message.h"
+#include "../include/message.h"
+#include <iostream>
+
+Message::Message(std::string type, std::string sender, std::string receiver, std::string time)
+{
+    this->type = type;
+    this->sender = sender;
+    this->receiver = receiver;
+    this->time = time;
+}
+
+Message::~Message()
+{
+}
+
+std::string Message::get_type() const
+{
+    return this->type;
+}
+
+std::string Message::get_sender() const
+{
+    return this->sender;
+}
+
+std::string Message::get_receiver() const
+{
+    return this->receiver;
+}
+
+std::string Message::get_time() const
+{
+    return this->time;
+}
+
+void Message::print(std::ostream &os) const
+{
+    
+}
+
+std::ostream &operator<<(std::ostream &os, const Message &msg)
+{
+    os <<"*************************\n"<<
+    msg.sender<<" -> "<<msg.receiver<<"\n"<<
+    "message type: "<<msg.type<<"\n"<<
+    "message time: "<<msg.time<<"\n"<<
+    "*************************\n";
+    return os;
+}
